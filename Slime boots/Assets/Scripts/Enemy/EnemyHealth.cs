0b8b76c961232d3c3c07a3e454df420971b54416
Fouad Mannou <36111622+fouadmen@users.noreply.github.com>
2020-11-02 13:58:37 +0900

    Inhert from Health class

--- Slime boots/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Slime boots/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,58 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 using TMPro;
+using Health;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : Health
 {
-    #region Public Instances
-    public GameObject Enemy;
-    public GameObject En_DeathParticle;
-
-    public TextMeshProUGUI NumOfHappyPower;
-    public TextMeshProUGUI NumOfSadPower;
-    #endregion
-
-    #region HapAndSad System Variables
-    public int enemySadnesPower;
-    [HideInInspector] public int currentHappinessPower;
-    [HideInInspector] public int currentSadnesPower;
-    #endregion
-
-    #region HapAndSad States
-    private bool isHappy = false;
-    private bool isSad = true;
-    #endregion
-
-    void Start()
-    {
-        currentSadnesPower = enemySadnesPower;
-        NumOfHappyPower.enabled = false;
-    }
-
-    void Update()
-    {
-        HapAndSadPowerSystem();
-        StartCoroutine(CheckThePlayerSadState());
-    }
-
     void HapAndSadPowerSystem()
     {
-        if (currentSadnesPower > 0)
+        if (entitySadnessPower > 0)
         {
-            NumOfSadPower.text = currentSadnesPower.ToString();
+            NumOfSadPower.text = entitySadnessPower.ToString();
         }
 
-        else if (currentSadnesPower == 0)
+        else if (entitySadnessPower == 0)
         {
             NumOfHappyPower.enabled = true;
             NumOfSadPower.enabled = false;
 
         }
 
-        else if (currentSadnesPower < 0 && currentHappinessPower == 0)
+        else if (entitySadnessPower < 0 && currentHappinessPower == 0)
         {
             isHappy = true; isSad = false;
-            currentHappinessPower = currentSadnesPower * -1;
+            currentHappinessPower = entitySadnessPower * -1;
             NumOfHappyPower.enabled = true;
             NumOfSadPower.enabled = false;
             NumOfHappyPower.text = currentHappinessPower.ToString();
@@ -64,15 +34,4 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
-    IEnumerator CheckThePlayerSadState()
-    {
-        if (isSad == false && isHappy == true)
-        {
-            yield return new WaitForSeconds(2f);
-            GameObject PlayerDeath_particle_prefab = Instantiate(En_DeathParticle, Enemy.transform.position, Quaternion.identity);
-
-            Destroy(PlayerDeath_particle_prefab.gameObject, 1f);
-            Destroy(Enemy.gameObject);
-        }
-    }
 }
